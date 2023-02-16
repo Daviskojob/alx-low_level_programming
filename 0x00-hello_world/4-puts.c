ed51e87adaf9a16b43dac8a673093628251d9771
Daviskojob <daviskojob@gmail.com>
2023-02-16 14:16:14 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - print "Programming is like building a multilingual 
- * puzzle, followed by a newline."
- * return 0.
+ * main - Entry
+ * Return: Always
  */
-
- int main(void)
+int main(void)
 
 {
-	puts("\""Programming is like building a multilingual puzzle");
-
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
